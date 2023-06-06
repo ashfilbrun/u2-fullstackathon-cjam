3fbeae646f2e9fb51deb9f0328abbfd52113cfc7
ashfilbrun <ashesclaire@gmail.com>
2023-06-06 12:14:55 -0700

    add headphone data in models

---
+++ models/headphone.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose')
+
+const headphoneSchema = new Schema(
+  {
+    image: { type: Image, required: true },
+    brand: { type: String, required: true },
+    model_name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    in_stock: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+)
+
+module.exports = headphoneSchema
